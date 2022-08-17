237f62e96310a79c7055ae1cafb254e409574109
ValGab <valentin.gabo@gmail.com>
2022-08-17 12:17:01 +0200

    change port

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.all("*", (req, res) => {
   res.status(404).json({ message: "Route introuvable" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server has started");
 });
